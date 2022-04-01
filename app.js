6d2ca17380715fc5bc4df3e80945be38e2a73659
Farhat Mahi <farhatmahi007@gmail.com>
2022-04-01 17:46:03 +0600

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+/*
+1. DOM selection
+2. Event Listener
+3. Basic Validation
+4. Creating Element
+5. Append
+*/
+
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const year = document.querySelector('#year');
+const btn = document.querySelector('.btn');
+const bookList = document.querySelector('#book-list');
+
+btn.addEventListener("click", function(e){
+    e.preventDefault(); 
+
+    if(title.value == "" && author.value == "" && year.value == ""){
+        alert("Bhai kisu ekta den") 
+    }
+    else{
+        const newRow = document.createElement('tr');
+        
+        //creating new title
+        const newTitle = document.createElement('th');
+        newTitle.innerHTML = title.value;
+        newRow.appendChild(newTitle);
+
+        //creating new author
+        const newAuthor = document.createElement('th');
+        newAuthor.innerHTML = author.value;
+        newRow.appendChild(newAuthor);
+
+        //creating new Year
+        const newYear = document.createElement('th');
+        newYear.innerHTML = year.value;
+        newRow.appendChild(newYear);
+
+        //append
+        bookList.appendChild( newRow)
+    }
+}) 
